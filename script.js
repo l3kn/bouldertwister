8e004a85e36f5ee820461b6f01cc14d3d2691d46
Leon Rische <leon.rische@student.uni-siegen.de>
2016-11-10 16:25:20 +0100

    m

--- script.js
+++ script.js
@@ -10,21 +10,18 @@ var element2 = document.getElementById("color")
 var inputs = document.getElementsByTagName("input")
 
 function roll() {
-  var colors = [];
+  var colors = []
 
   for (var i = 0; i < inputs.length; i++) {
     var current = inputs[i]
     if (current.checked) {
-      colors.push(current.value);
+      colors.push(current.value)
     }
   }
 
-  var random = Math.floor(Math.random() * 4);
-  element1.innerHTML = bodyparts[random];
+  var random = Math.floor(Math.random() * 4)
+  element1.innerHTML = bodyparts[random]
 
-  var random = Math.floor(Math.random() * colors.length);
-  element2.innerHTML = colors[random];
+  var random = Math.floor(Math.random() * colors.length)
+  element2.innerHTML = colors[random]
 }
-
-document.onclick = roll;
-document.ontouchstart = roll;
