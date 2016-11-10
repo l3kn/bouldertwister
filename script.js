1e73289e58ecfb2d14051c9f4815deec1a8b518e
Leon Rische <leon.rische@student.uni-siegen.de>
2016-11-10 15:40:12 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+var bodyparts = [
+  "rechte Hand",
+  "linke Hand",
+  "rechter Fuß",
+  "linker Fuß"
+]
+
+var element1 = document.getElementById("bodypart")
+var element2 = document.getElementById("color")
+var inputs = document.getElementsByTagName("input")
+console.log(element1);
+
+function roll() {
+  var colors = [];
+
+  for (var i = 0; i < inputs.length; i++) {
+    var current = inputs[i]
+    if (current.checked) {
+      colors.push(current.value);
+    }
+  }
+
+  var random = Math.floor(Math.random() * 4);
+  element1.innerHTML = bodyparts[random];
+
+  var random = Math.floor(Math.random() * colors.length);
+  element2.innerHTML = colors[random];
+}
